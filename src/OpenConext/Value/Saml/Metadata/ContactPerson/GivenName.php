d342ad5e9fb1ca7ca543453ced85d8279a021882
DRvanR <dvrenterghem@ibuildings.nl>
2016-02-22 16:07:15 +0100

    Implemented GivenName Value Object

---
+++ src/OpenConext/Value/Saml/Metadata/ContactPerson/GivenName.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OpenConext\Value\Saml\Metadata\ContactPerson;
+
+use OpenConext\Value\Exception\InvalidArgumentException;
+
+final class GivenName
+{
+    /**
+     * @var string
+     */
+    private $givenName;
+
+    /**
+     ** @param string $givenName
+     */
+    public function __construct($givenName)
+    {
+        if (!is_string($givenName) || trim($givenName) === '') {
+            throw InvalidArgumentException::invalidType('non-blank string', 'givenName', $givenName);
+        }
+
+        $this->givenName = $givenName;
+    }
+
+    /**
+     * @param GivenName $other
+     * @return bool
+     */
+    public function equals(GivenName $other)
+    {
+        return $this->givenName === $other->givenName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGivenName()
+    {
+        return $this->givenName;
+    }
+
+    public function __toString()
+    {
+        return $this->givenName;
+    }
+}
---
+++ tests/OpenConext/Value/Saml/Metadata/ContactPerson/GivenNameTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace OpenConext\Value\Saml\Metadata\ContactPerson;
+
+use OpenConext\Value\Exception\InvalidArgumentException;
+use PHPUnit_Framework_TestCase as UnitTest;
+
+class GivenNameTest extends UnitTest
+{
+    /**
+     * @test
+     * @group        metadata
+     * @group        contactperson
+     *
+     * @dataProvider \OpenConext\Value\TestDataProvider::notStringOrEmptyString
+     * @expectedException InvalidArgumentException
+     *
+     * @param mixed $invalidValue
+     */
+    public function only_non_empty_strings_are_valid_given_names($invalidValue)
+    {
+        new GivenName($invalidValue);
+    }
+
+    /**
+     * @test
+     * @group metadata
+     * @group contactperson
+     */
+    public function equality_can_be_verified()
+    {
+        $base      = new GivenName('Homer');
+        $theSame   = new GivenName('Homer');
+        $different = new GivenName('Marge');
+
+        $this->assertTrue($base->equals($theSame));
+        $this->assertFalse($base->equals($different));
+    }
+
+    /**
+     * @test
+     * @group metadata
+     * @group contactperson
+     */
+    public function the_given_name_can_be_retrieved()
+    {
+        $name = 'Homer';
+
+        $givenName = new GivenName($name);
+
+        $this->assertEquals($name, $givenName->getGivenName());
+    }
+
+    /**
+     * @test
+     * @group metadata
+     * @group contactperson
+     */
+    public function a_given_name_can_be_cast_to_string()
+    {
+        $name = 'Homer';
+
+        $givenName = new GivenName($name);
+
+        $this->assertEquals($name, (string) $givenName);
+    }
+}
